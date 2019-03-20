94f434f524227d0ece36bb10cc7e97777c9cc7df
sagar949 <vidya.pamula@gmail.com>
2019-03-20 23:09:41 +0530

    Create local users in server

---
+++ CreateSingleUser.ps1
@@ -0,0 +1,17 @@
+$password = ConvertTo-SecureString "password@123" -AsPlainText -Force
+
+# Group new users are added to (default is Users)
+$group = "Users"
+
+# PowerShell array containing the users to be created
+$users = @(
+    "Margret"
+)
+
+# Creates a new users for each entry in the $users array
+foreach ($user in $users) {
+	
+    # Create and add user to Users group
+    New-LocalUser -Name "$user" -Password $Password -Description "usergroup standard User" -PasswordNeverExpires:$true 
+    Add-LocalGroupMember -Group "$group" -Member "$user"
+}
